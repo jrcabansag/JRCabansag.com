b44928c1d7c34850fca795861ec512b16044d13b
JR <cabansag@stanford.edu>
2017-10-25 17:01:49 -0700

    Cleaned up style and organized all files into folders

---
+++ js/mobile-redirect.js
@@ -0,0 +1,5 @@
+var md = new MobileDetect(window.navigator.userAgent);
+
+if(md.mobile() != null){
+    window.location.replace("http://jrcabansag.com/mobile.html");
+}
---
+++ js/website-animate.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    var s = skrollr.init();
+
+    $("#AboutMeButton").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#AboutMe").offset().top
+        }, 300);
+    });
+    $("#ProjectsButton").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#Projects").offset().top
+        }, 300);
+    });
+    $("#LogoCircle").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#Intro").offset().top
+        }, 300);
+    });
+
+    $(window).scroll(function () {
+        if($(window).scrollTop()+$(window).height()/2 > $('#AboutMe').offset().top && $(window).scrollTop()+$(window).height()/2 < $("#Projects").offset().top) {
+            $("#AboutMeButton").css("color","#F2DE5F");
+        }else{
+            $("#AboutMeButton").css("color","");
+        }
+        if($(window).scrollTop()+$(window).height()/2 > $('#Projects').offset().top) {
+            $("#ProjectsButton").css("color","#F2DE5F");
+        }else{
+            $("#ProjectsButton").css("color","");
+        }
+    });
+});
+
+var photoIndex = -1;
+
+function changePhoto(){
+    photoIndex += 1;
+    var photoNumber = photoIndex%5+1;
+    var nextPhotoNumber = photoNumber+1;
+    if(nextPhotoNumber > 5){
+        nextPhotoNumber = 1;
+    }
+    $('#AboutMeImage'+photoNumber).fadeOut(250);
+    $('#AboutMeImage'+nextPhotoNumber).fadeIn(300);
+}
